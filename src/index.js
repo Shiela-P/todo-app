c8782736e5585bdb1a3bb0a104bd5f9c3c2762bf
Shiela-P <125091812+Shiela-P@users.noreply.github.com>
2024-05-20 20:23:46 -0400

    Rendered my first homemade element to the screen!

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 // required import statements
-import reacteact from 'react';
-import Reactdom from 'react-dom/client';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+//Element. Does not render to screen until render is called
+const testElement = <h1>My Todo List</h1>
+const testElement2 = <h2>Made with React</h2>
+
+//Making root (where everything renders)
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+//render to root using render
+root.render(testElement);
+
+
+
+
+
+
+
+
+
